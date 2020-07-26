c9e92464449fffbfad851876c3fceebfd3f05e81
Subiyel ijaz <subialijaz@gmail.com>
2020-07-26 20:43:00 +0500

    redux-logger

--- src/store/configureStore.js
+++ src/store/configureStore.js
@@ -1,8 +1,10 @@
-import { createStore } from 'redux'
+import { createStore, applyMiddleware } from 'redux'
 import { persistStore, persistReducer } from 'redux-persist'
 import storage from 'redux-persist/lib/storage' // defaults to localStorage for web
 import AsyncStorage from '@react-native-community/async-storage';
 import appReducer from './reducers/appReducer'
+import logger from 'redux-logger'
+
 
 const persistConfig = {
   key: 'root',
@@ -12,7 +14,7 @@ const persistConfig = {
 const persistedReducer = persistReducer(persistConfig, appReducer)
 
 export default () => {
-  let store = createStore(persistedReducer)
+  let store = createStore(persistedReducer, applyMiddleware(logger))
   let persistor = persistStore(store)
   return { store, persistor }
 }
